26bc1087dfebee91fe82d26b34b865977a924f51
Trần Đại Dũng <101490800+dung28-td@users.noreply.github.com>
2022-10-16 12:18:19 +0700

    Bundle uploading

---
+++ scripts/upload_bundle.js
@@ -0,0 +1,51 @@
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getStorage } = require('firebase-admin/storage');
+const { readdirSync } = require('fs');
+
+const serviceAccount = require('../secrets/firebase_sak.json');
+
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: `${serviceAccount.project_id}.appspot.com`
+});
+
+const bucket = getStorage().bucket();
+const version = process.env.GITHUB_REF_NAME
+
+const uploadBundle = (file, destination) => {
+  console.log("Start upload", file)
+
+  return bucket.upload(file, {
+    gzip: true,
+    destination,
+    ...(file.endsWith('.bundle') && {
+      metadata: {
+        contentType: 'text/javascript'
+      }
+    })
+  })
+    .then(() => console.log(`Upload file "${file}" successfully!`))
+    .catch(err => console.error(`Failed to upload file "${file}"`, err))
+}
+
+const uploadBundles = platform => {
+  const dir = `build/output/${platform}/remote`
+
+  return Promise.all(readdirSync(dir).map(filename => {
+    const file = `${dir}/${filename}`
+    const destination = `${version}/${platform}/${filename}`
+
+    return uploadBundle(file, destination)
+  }))
+}
+
+const main = async () => {
+  console.log('Start upload bundles')
+
+  await uploadBundles('ios'),
+  await uploadBundles('android')
+
+  console.log("Upload bundles successfully!")
+}
+
+main()
